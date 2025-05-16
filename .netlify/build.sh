5cd02d9718f977371d34cb78671816301f2b2fc1
Przemek <przemyslaw.kalka@gmail.com>
2025-05-16 10:58:36 +0100

    refactor: remove Playwright dependency installation from Netlify build script

--- .netlify/build.sh
+++ .netlify/build.sh
@@ -4,9 +4,6 @@ set -eux
 # Install your deps, skipping Playwright’s browsers
 npm ci
 
-# Install Playwright’s dependencies (fonts, libs) but NOT browsers
-npx playwright install-deps
-
-# Build and test
+# Build and test (no playwright install-deps, as Netlify does not allow root)
 npm run build
 npx playwright test --config=playwright.dist.config.ts
